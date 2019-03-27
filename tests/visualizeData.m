b9d1bb14e06c688d22ba8e3a282cdd69261218f8
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-27 14:39:50 -0700

    added code to load the dataset and plot the data in tests/visualizeData.m

--- tests/visualizeData.m
+++ tests/visualizeData.m
@@ -1,3 +1,20 @@
 % Initialization
 clear; close all; clc;
 
+printf("Visualizing example dataset.\n\n");
+
+% Load the dataset
+% Now contains X, Xval, and yval
+load("../data/data1.mat");
+printf("Loaded dataset into X, Xval, and yval\n");
+
+% Visualize the example dataset
+printf("Plotting latency vs Throughput as blue crosses\n");
+plot(X(:, 1), X(:, 2), 'bx');
+axis([0 30 0 30]);
+xlabel("Latency (ms)");
+ylabel("Throughput (mb/s)");
+
+% Print into figures
+% printf("Saving figure into figures/figure1.jpg\n");
+% print -djpg ../figures/figure1.jpg
