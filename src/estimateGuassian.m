35b5531130b314bebc00fc30cd38a0c68e788e63
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-27 20:02:45 -0700

    added documentation in src/estimateGuassian.m

--- src/estimateGuassian.m
+++ src/estimateGuassian.m
@@ -1,3 +1,10 @@
 function [mu sigma2] = estimateGuassian(X)
+%ESTIMATEGUASSIAN estimates the parameters of a 
+%Guassian distribution using the data in X
+%   [mu sigma2] = estimateGuassian(X),
+%   The input X is the dataset with each n-dimensional data
+%   point in one row. The output is an n-dimensional vector mu,
+%   the mean of the data set and the variance and the variances,
+%   sigma2, an n x 1 vector
 
 end
