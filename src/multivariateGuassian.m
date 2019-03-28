433b681900ef4127ca344cf5bc8f834329122e28
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-27 20:17:25 -0700

    added src/multivariateGuassian.m

---
+++ src/multivariateGuassian.m
@@ -0,0 +1,13 @@
+function p = multivariateGuassian(X, mu, Sigma2)
+%MULTIVARIATEGUASSIAN Computes the probability density
+%function of the multivariate guassian distribution
+%   p = MULTIVARIATEGUASSIAN(X, mu Sigma2) Computes the probability
+%   density function of the examples X under the multivariate
+%   guassian distribution with parameters mu and Sigma2. If Sigma2 is a 
+%   matrix, it is treated as the covariance matrix. If Sigma2 is a vector,
+%   it is treated as the sigma^2 values of the variances in each
+%   dimension (a diagonal covariance matrix)
+%
+
+
+end
